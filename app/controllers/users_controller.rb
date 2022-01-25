31d06268068f7f765285ac0b3de04b45ba0a18b4
akash <akashs3096@gmail.com>
2022-01-25 11:16:01 +0530

    adding users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.create(
+      name: params[:name],
+      email: params[:email],
+      username: params[:username],
+      password: params[:password]
+      )
+    render json: user
+  end
+
+end
